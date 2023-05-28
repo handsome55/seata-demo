81412d16e5364fa75b4eca452559f115992fa837
wubixuan <wubixuan@qq.com>
2023-05-28 21:29:05 +0800

    添加sql

---
+++ sql/order_tbl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `order_tbl` (
+                             `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                             `order_no` varchar(128) NOT NULL COMMENT '订单号',
+                             `user_id` bigint(11) DEFAULT NULL COMMENT '用户id',
+                             `product_id` bigint(11) DEFAULT NULL COMMENT '产品id',
+                             `count` int(11) DEFAULT NULL COMMENT '数量',
+                             `money` decimal(11,2) DEFAULT NULL COMMENT '金额',
+                             `status` int(1) DEFAULT NULL COMMENT '订单状态：0：创建中；1：已完结',
+                             PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
