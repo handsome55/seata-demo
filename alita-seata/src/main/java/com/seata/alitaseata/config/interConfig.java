a6233b7e2d1be8ef9569e28322ea15407773f551
wubixuan <wubixuan@qq.com>
2023-06-08 17:41:41 +0800

    添加拦截器可以传递XID

---
+++ alita-seata/src/main/java/com/seata/alitaseata/config/interConfig.java
@@ -0,0 +1,26 @@
+package com.seata.alitaseata.config;
+
+import io.seata.integration.grpc.interceptor.client.ClientTransactionInterceptor;
+import io.seata.integration.grpc.interceptor.server.ServerTransactionInterceptor;
+import net.devh.boot.grpc.client.interceptor.GrpcGlobalClientInterceptor;
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+
+/**
+ * TODO
+ *
+ * @author WUBX
+ * @date 2023/6/8 17:40
+ */
+public class interConfig {
+
+    @GrpcGlobalClientInterceptor
+    public ClientTransactionInterceptor clientTransactionInterceptor(){
+        return new ClientTransactionInterceptor();
+    }
+
+    @GrpcGlobalServerInterceptor
+    public ServerTransactionInterceptor serverTransactionInterceptor(){
+        return new ServerTransactionInterceptor();
+    }
+
+}
